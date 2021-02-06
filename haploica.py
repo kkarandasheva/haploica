bc07e611fcc1416179c8080b1c30af883df21af2
kkarandasheva <christinavader@gmail.com>
2021-02-06 19:16:57 +0300

    Hotspot variant calling via TVC added.

--- haploica.py
+++ haploica.py
@@ -3,6 +3,8 @@ import sys
 import os
 import pandas
 import pybedtools
+import subprocess
+import tempfile
 
 def parse_options():
     """
@@ -95,7 +97,7 @@ def read_parameters(filename):
     with open(filename, 'r') as fn:
         for line in fn:
             x = line.rstrip("\r\n").split('=')
-            parameters[x[0]] = eval(x[1]) if x[1].startswith('[') and x[1].endswith(']') else x[1] # TODO: FIX INJECT DANGER
+            parameters[x[0]] = eval(x[1]) # TODO: FIX INJECT DANGER
     return parameters
 
 
@@ -115,6 +117,7 @@ def read_snv(filename, af=0.01):
     df = df[df.columns[[0,1,2,3,4,5,9,10,14,17]]]
     df.columns = 'Chromosome Start End Name Score Strand Ref Alt AF RS'.split(' ')
     df_filtered = df[df['AF'] > af]
+    df_filtered = df_filtered.drop_duplicates()
     return df_filtered
 
 
@@ -127,8 +130,63 @@ def filter_snv(snv, design):
     """
     bt_snv = pybedtools.BedTool.from_dataframe(snv)
     bt_dsgn = pybedtools.BedTool.from_dataframe(design)
-    result = bt_snv.intersect(bt_dsgn)
+    bt_intersect = bt_snv.intersect(bt_dsgn)
+    result = bt_intersect.to_dataframe()
+    result.columns = snv.columns
     return result
+
+
+def hotspot_write(df, outfile):
+    """
+    Write hotspot.vcf into file (tmp)
+    :param df:
+    :param outfile:
+    :return:
+    """
+    f = open(outfile, 'w')
+    f.write('##fileformat=VCFv4.1\n##allowBlockSubstitutions=true\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n')
+    for index, row in df.iterrows():
+        newline = [row['Chromosome'], str(row['Start']), '.', row['Ref'], row['Alt'], '.', '.',
+                   'OID=ID{};OPOS={};OREF={};OALT={};OMAPALT={}'.format(index+1, row['Start'], row['Ref'], row['Alt'], row['Alt'])]
+        newline = '\t'.join(newline) + '\n'
+        f.write(newline)
+    return
+
+
+def tvc_hotspot(bam, hotspot, tvc, reference, errormotifs, params, bed, outdir):
+    hotspot_write(df=hotspot, outfile=outdir+'/hotspot.vcf')
+    cmd = ['python', tvc,
+           '--reference-fasta', reference,
+           '--hotspot-vcf', outdir+'hotspot.vcf',
+           '--error-motifs', errormotifs,
+           '--input-bam', bam,
+           '--parameters-file', params,
+           '--region-bed', bed,
+           '--output-dir', outdir,
+           '--primer-trim-bed', bed]
+    devnull = open(os.devnull, 'w')
+    #subprocess.call(cmd, stdout=devnull, stderr=devnull)
+    hotspot_out = outdir+'TSVC_variants.vcf'
+    df = pandas.read_csv(hotspot_out, sep='\t', comment='#', names=['Chrom', 'Start', 'ID', 'Ref', 'Alt', 'Qual', 'Filter', 'Format', 'Info', 'Sample'])
+    df[df['Info'][1].split(':')] = df['Sample'].str.split(':', expand=True)
+    df = df[['Chrom', 'Start', 'ID', 'Ref', 'Alt', 'Filter', 'DP', 'AF']]
+    df_splitted = pandas.DataFrame(columns = df.columns)
+    for _, row in df.iterrows():
+        if ';' in row['ID']:
+            x = pandas.DataFrame({'Chrom': [row['Chrom']] * len(row['ID'].split(';')),
+                                  'Start': [row['Start']] * len(row['ID'].split(';')),
+                                  'ID': row['ID'].split(';'),
+                                  'Ref': [row['Ref']] * len(row['ID'].split(';')),
+                                  'Alt': row['Alt'].split(',') if ',' in row['Alt'] else [row['Alt']] * len(row['ID'].split(';')),
+                                  'Filter': [row['Filter']] * len(row['ID'].split(';')),
+                                  'DP': [row['DP']] * len(row['ID'].split(';')),
+                                  'AF': row['AF'].split(',') if ',' in row['AF'] else [row['AF']] * len(row['ID'].split(';'))})
+            df_splitted = pandas.concat([df_splitted, x]).reset_index(drop=True)
+        else:
+            df_splitted = df_splitted.append(row).reset_index(drop=True)
+    df_splitted = df_splitted[df.columns]
+    df_splitted_sorted = pandas.DataFrame(sorted(df_splitted.values, key=lambda x: int(x[2].replace('ID', ''))), columns=df.columns)
+    return df_splitted_sorted
 
 
 def main(infile, outdir, format, design, paramfile, errorhandler):
@@ -149,6 +207,20 @@ def main(infile, outdir, format, design, paramfile, errorhandler):
     design = read_design(filename=design)
     snv = pandas.concat(map(read_snv, parameters['SNV'])) if isinstance(parameters['SNV'], list) else read_snv(parameters['SNV'])
     snv = filter_snv(snv=snv, design=design)
+
+    if parameters['VC'] == "TVC":
+        k = 3
+        hotspot = tvc_hotspot(bam=infile,
+                    hotspot=snv,
+                    tvc=parameters['TVCPATH'],
+                    reference=parameters['TVCREF'],
+                    bed=parameters['TVCBED'],
+                    errormotifs=parameters['TVCERR'],
+                    params=parameters['TVCPAR'],
+                    outdir=outdir) # TODO: uncomment subprocess
+    k = 3
+    sample = draw_haplotypes(hotspot, gene='All')
+
 
 if __name__ == '__main__':
     options = parse_options()
